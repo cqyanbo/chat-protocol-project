f6bdf564f08c384b41bae62eca038add0ece7f1a
cqyanbo <cqyanbo@f631683f-39cc-de18-a0e5-884a5ae83aea>
2012-06-06 12:05:26 +0000

    add timeout 10 minutes for each connection

--- trunk/Nebula/src/Server/ChatServer.java
+++ trunk/Nebula/src/Server/ChatServer.java
@@ -34,7 +34,7 @@ public class ChatServer{
 			while(true)
 			{
 				Socket incoming = serverSocket.accept();
-				//incoming.setSoTimeout(60000);
+				incoming.setSoTimeout(600000);
 				System.out.println("connected with " + incoming.getInetAddress());
 				
 				// spawn a thread to handle the request
